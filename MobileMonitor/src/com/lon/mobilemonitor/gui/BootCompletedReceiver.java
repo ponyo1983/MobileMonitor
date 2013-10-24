475e3b1f468e7a3fc1d54b72e10ecdb8cc614314
Administrator <Administrator@John-Notebook>
2013-10-24 09:12:59 +0800

    加入开机自动启动功能

---
+++ MobileMonitor/src/com/lon/mobilemonitor/gui/BootCompletedReceiver.java
@@ -0,0 +1,19 @@
+package com.lon.mobilemonitor.gui;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+			Intent newIntent = new Intent(context, MainActivity.class);
+			newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // 注意，必须添加这个标记，否则启动会失败
+			context.startActivity(newIntent);
+		}
+	}
+
+}
